080d5abe5ec66c1fd99733592f35afc31fc82e54
BilguunLeap3 <bilguunleap3BilguunLeap3@iMAC2021-05s-iMac.local>
2023-03-30 12:21:28 +0800

    email

--- emailsign/index.js
+++ emailsign/index.js
@@ -1,43 +1,64 @@
-const {DynamoDB} = require('@aws-sdk/client-dynamodb');
-const {marshall, unmarshall} = require('@aws-sdk/util-dynamodb');
-const db = new DynamoDB();
-const nodemailer = require('nodemailer');
-const bcrypt = require('bcryptjs');
-const smtpTransport = require('nodemailer-smtp-transport');
-module.exports.signup = async (event) => {
-  async function hashPass(pass) {
-    return await bcrypt.hash(pass, 1);
+// const {DynamoDB} = require('@aws-sdk/client-dynamodb');
+// const {marshall, unmarshall} = require('@aws-sdk/util-dynamodb');
+// const db = new DynamoDB();
+// const bcrypt = require('bcryptjs');
+const axios = require('axios')
+// module.exports.signup = async (event) => {
+//   const {username, email, password} = JSON.parse(event.body);
+//   const userid = '1';
+//   async function hashPass(pass) {
+//     return await bcrypt.hash(pass, 1);
+//   }
+//   const response = await db.putItem({
+//     TableName: 'users',
+//     Item: marshall({
+//       userid,
+//       username,
+//       email,
+//       password: await hashPass(password),
+//     })
+//   });
+//   return {
+//     'body': 'amjilttai nemlee'
+//   };
+// };
+// module.exports.signin = async (event) => {
+//   const {email, password} = JSON.parse(event.body);
+//   const {Items} = await db.query({
+//     TableName: 'users',
+//     KeyConditionExpression: "email = :email",
+//     IndexName: "email-index",
+//     ExpressionAttributeValues: {
+//       ":email": {S: email},
+//     }
+//   });
+//   const user = unmarshall(Items[0]);
+//   const isPasswordRight = await bcrypt.compare(password, user.password);
+//   if(isPasswordRight) {
+//     return {
+//       'body': 'true',
+//     };
+//   }else{
+//     return {
+//       'body': 'false',
+//     };
+//   }
+  
+// };
+
+async function a() {
+try {
+ const res = await axios.post('https://c1c31nztud.execute-api.us-east-1.amazonaws.com/dev/signin', {
+  email: '1@yahoo.com',
+  password: '123456',
+},{
+  headers: {
+    'Content-Type': 'application/json',
   }
-  const response = await db.putItem({
-    TableName: 'users',
-    Item: marshall({
-      userid: '1',
-      firstname: 'Bilguun',
-      lastname: 'Zorigoo',
-      email: 'bizozobi30@gmail.com',
-      password: await hashPass('123456'),
-    })
-  });
-  return '1';
-};
-module.exports.sendemail = async (event) => {
-  const email = event?.Records[0]?.dynamodb?.NewImage?.email?.S;
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: "bizozobi30@gmail.com", // generated ethereal user
-      pass: "qhuadcltdaboceoe", // generated ethereal password
-    },
-  });
-  //qhuadcltdaboceoe
-  let mailOptions = {
-    from: 'bizozobi30@gmail.com',
-    to: email,
-    subject: 'Sending Email using Node.js[nodemailer]',
-    text: 'That was easy!'
-  };
-  const info = await transporter.sendMail(mailOptions)
-  return info.messageId;
-};
+});
+console.log(res)
+} catch (error) {
+  console.log(error.message)
+}
+}
+a()
