ad60e22b3293a048985f51d6f11ae230676e1201
ubuntu <zorigoobilguun30@gmail.com>
2023-04-03 19:29:09 +0800

    learned s3 serverless.yml

--- s3bucket/index.js
+++ s3bucket/index.js
@@ -1,16 +1,20 @@
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
-const uuid = require('uuid');
+const Aws = require('aws-sdk');
+const S3 = new Aws.S3();
+const {v4: uuidv4} = require('uuid');
 module.exports.handler = async (event) => {
-  const Key = `asd.png`
-  const s3Params = {
-    Bucket: `leap3devbilguunzs3`,
-    Key: Key,
-    ContentType: 'image/png'
-  }
-  const uploadURL = s3.getSignedUrl('putObject', s3Params)
-  return JSON.stringify({
-    uploadURL: uploadURL,
-    Key
-  })
+  const params = {
+    Bucket: 'zorigoobilguunleap3',
+    Key: `${uuidv4()}.png`,
+    ContentType: 'image/png',
+  };
+  const uploadUrl = S3.getSignedUrl('putObject',params);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        event,
+        uploadUrl,
+      },
+    ),
+  };
 };
